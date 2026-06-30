b530d2d66f8c311ed0636a8e3c41b902b68f81ef
agarovshen <agarovshen@gmail.com>
2026-07-01 04:27:08 +0500

    Improve currency input add rate output

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from api import get_rates_by_name
 from printer import print_rates
 def main():
     print("=== BESTCHANGE PARSER ===")
-    from_code = input("From currency: ")
-    to_code = input("To currency: ")
+    from_code = input("From currency: ").upper()
+    to_code = input("To currency: ").upper()
     rates = get_rates_by_name(from_code,to_code)
-    print_rates(rates)
+    for r in rates:
+        changer = r.get('changer')
+        rate = r.get('rate')
+        inmin = r.get('inmin')
+        inmax = r.get('inmax')
+        print(f"Changer: {changer} |rate: {rate} |imin: {inmin} |imax: {inmax}")
     print ("\n Ready \n")
 main()
