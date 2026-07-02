37005901de4d2bd82492f7824cde60facd85bf42
agarovshen <agarovshen@gmail.com>
2026-07-03 03:59:08 +0500

    Add GUI currency input

--- gui.py
+++ gui.py
@@ -1,8 +1,32 @@
 import tkinter as tk
-def run_app():
+from api import get_rates_by_name
+
+def get_currency_codes():
     root = tk.Tk()
     root.geometry("400x300")
     root.title("Arbitrage Bot")
-    label = tk.Label(root, text="Hello Gui")
-    label.pack()
+
+    #From currency label
+    tk.Label(root, text="From e.g. BTC: ").pack()
+    from_input = tk.Entry(root)
+    from_input.pack()
+
+    #To currency label
+    tk.Label(root, text=("To e.g. USDT: ")).pack()
+    to_input = tk.Entry(root)
+    to_input.pack()
+
+    #Output label
+    output = tk.Label(root, text="")
+    output.pack()
+    result = {}
+    def on_click():
+        result["from_code"] = from_input.get().upper()
+        result["to_code"] = to_input.get().upper()
+        # output.config(text=f"Loading {result['from_code']} -> {result['to_code']} ...")
+        root.destroy()
+    
+    btn = tk.Button(root, text="GET RATES", command=on_click)
+    btn.pack()
     root.mainloop()
+    return result["from_code"], result["to_code"]
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from api import get_rates_by_name
 from printer import print_changer
 from logic import analyze_rates
-from gui import run_app
+from gui import get_currency_codes
 def main():
-    from_code = input("From currency: ").upper()
-    to_code = input("To currency: ").upper()
+    from_code, to_code = get_currency_codes()
     rates = get_rates_by_name(from_code,to_code)
     result = analyze_rates(rates)
     for r in result:
         print_changer(r)
-    run_app()
 main()
