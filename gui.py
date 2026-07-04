bc8eaf85af4c67562c3a061a49b8c6ddac7fc578
agarovshen <agarovshen@gmail.com>
2026-07-05 02:26:18 +0500

    Implement single window tkinter interface

--- gui.py
+++ gui.py
@@ -3,49 +3,49 @@ from formatter import format_changer
 
 def run_app(on_search):
     root = tk.Tk()
-    root.geometry("400x300")
+    root.geometry("600x700")
     root.title("Arbitrage Bot")
+    top_frame = tk.Frame(root)
+    top_frame.pack(fill="x", padx=10, pady=10)
 
     #From currency label
-    tk.Label(root, text="From e.g. BTC: ").pack()
-    from_input = tk.Entry(root)
-    from_input.pack()
+    tk.Label(top_frame, text="From e.g. BTC: ").pack(anchor="w", padx=5)
+    from_input = tk.Entry(top_frame)
+    from_input.pack(fill="x")
 
     #To currency label
-    tk.Label(root, text=("To e.g. USDT: ")).pack()
-    to_input = tk.Entry(root)
-    to_input.pack()
+    tk.Label(top_frame, text=("To e.g. USDT: ")).pack(anchor="w", padx=5)
+    to_input = tk.Entry(top_frame)
+    to_input.pack(fill="x")
 
     #Output label
-    output = tk.Label(root, text="")
-    output.pack()
-    result = {}
-
-    # #List of changers
-    # text = tk.Text(root, text)
-
-    def on_click():
-        result["from_code"] = from_input.get().upper()
-        result["to_code"] = to_input.get().upper()
-        # output.config(text=f"Loading {result['from_code']} -> {result['to_code']} ...")
-        # root.destroy()
-        # return result["from_code"], result["to_code"]
-    btn = tk.Button(root, text="GET RATES", command=on_click)
+    output = tk.Label(top_frame, text="")
+    output.pack()\
+    
+    #Button
+    btn = tk.Button(top_frame, text="GET RATES")
     btn.pack()
-    on_search("BTC", "LTC")
-    # from_code, to_code = on_click()
-    # print(from_code,to_code)
-    # tk.Label(root, text=(from_code,to_code)).pack()
-    # on_search(from_code, to_code)
+
+    #Bottom Frame
+    result_frame = tk.Frame(root)
+    result_frame.pack(fill="both", padx=10, pady=10)
+
+    #Text of list of changers
+    text = tk.Text(result_frame)
+    text.pack(fill="both")
+
+    #List of changers
+    def on_click():
+        from_code = from_input.get().upper()
+        to_code = to_input.get().upper()
+        result = on_search(from_code, to_code)
+        root.title("Exchange Rates")
+        output.config(text=f"Loading {from_code} -> {to_code} ...")
+        text.delete("1.0", "end")
+
+        for r in result:
+            text.insert("end", format_changer(r) + "\n")
+    
+    btn.config(command=on_click)
+
     root.mainloop()
-# def show_rates(rates):
-#     root = tk.Tk()
-#     root.geometry("800x500")
-#     root.title("Exchange Rates")
-
-#     text = tk.Text(root)
-#     text.pack(fill="both", expand=True)
-
-#     for r in rates:
-#         text.insert("end", format_changer(r) + "\n")    
-#     root.mainloop()
--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from api import get_rates_by_name
-# from formatter import format_changer
 from logic import analyze_rates
 from gui import run_app
 
-
-
 def main():
-    # from_code, to_code = run_app()
     def handle_search(from_code, to_code):
         rates = get_rates_by_name(from_code,to_code)
         result = analyze_rates(rates)
         result = result[:7]
-        print("fack you", result)
+        return result
     run_app(handle_search)
 main()
