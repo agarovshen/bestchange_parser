1e9ae04d7b97a3afa53210361306ce553af847af
agarovshen <agarovshen@gmail.com>
2026-07-20 03:51:08 +0500

    Adding SQLite storage for exchange directions

--- database.py
+++ database.py
@@ -13,10 +13,51 @@ def create_tables():
                    id INTEGER PRIMARY KEY AUTOINCREMENT,
                    from_code TEXT,
                    to_code TEXT,
-                   changer_name TEXT
+                   changer_name TEXT,
                    rate REAL,
                    exchange_rate REAL,
-                   inmin REAL)
+                   inmin REAL,
+                   reserve REAL)
                    """)
+    conn.commit()
+    conn.close()
+
+def check_table():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("PRAGMA table_info(rates)")
+
+    for column in cursor.fetchall():
+        print(column)
+    
+    conn.commit()
+    conn.close()
+def save_direction(direction):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    for rate in direction.rates:
+        cursor.execute("""
+            INSERT INTO rates(
+                       from_code,
+                       to_code,
+                       changer_name,
+                       rate,
+                       exchange_rate,
+                       inmin,
+                       reserve
+                       )
+                       VALUES(?,?,?,?,?,?,?)
+                       """,
+                       (
+                           direction.from_code,
+                           direction.to_code,
+                           rate["changer_name"],
+                           rate["rate"],
+                           rate["exchange_rate"],
+                           rate["inmin"],
+                           rate["reserve"]
+                       ))
     conn.commit()
     conn.close()
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from api import get_rates
 from logic import calculate_reverse_spreads
 from gui import run_app
-from database import create_tables
+from database import create_tables, save_direction, check_table
 
 def main():
     create_tables()
+    check_table()
     def handle_search(from_code, 
                       to_code, 
                       show_reverse_rates_enabled=False, 
                       calculate_spreads_enabled=False):
         direct_direction, reverse_direction = get_rates(from_code, to_code)
+        # print(f"main.py direct direction -> {direct_direction.rates} \n"
+        #       f"reverse direction --> {reverse_direction.rates}")
+        save_direction(direct_direction)
+        save_direction(reverse_direction)
         direct_direction.select_best(top=3)
         if show_reverse_rates_enabled or calculate_spreads_enabled:
             reverse_direction.select_cheapest(top=3)
