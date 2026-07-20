76167138f3f1ce722fdbb2b2953789bab3163086
agarovshen <agarovshen@gmail.com>
2026-07-20 06:57:50 +0500

    Refactor database funktions to Database class

--- database.py
+++ database.py
@@ -1,63 +1,119 @@
 import sqlite3
-DB_NAME = "rates.db"
+from models import ExchangeDirection
+class Database:
+    def __init__(self, db_name="rates.db"):
+        self.db_name = db_name
+    ##########################################    
+    def get_connection(self):
+        return sqlite3.connect(self.db_name)
+    ##########################################
+    def create_tables(self):
+        conn = self.get_connection()
+        cursor = conn.cursor()
 
-def get_connection():
-    return sqlite3.connect(DB_NAME)
-
-def create_tables():
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS rates (
-                   id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   from_code TEXT,
-                   to_code TEXT,
-                   changer_name TEXT,
-                   rate REAL,
-                   exchange_rate REAL,
-                   inmin REAL,
-                   reserve REAL)
-                   """)
-    conn.commit()
-    conn.close()
-
-def check_table():
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("PRAGMA table_info(rates)")
-
-    for column in cursor.fetchall():
-        print(column)
-    
-    conn.commit()
-    conn.close()
-def save_direction(direction):
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    for rate in direction.rates:
         cursor.execute("""
-            INSERT INTO rates(
-                       from_code,
-                       to_code,
-                       changer_name,
-                       rate,
-                       exchange_rate,
-                       inmin,
-                       reserve
-                       )
-                       VALUES(?,?,?,?,?,?,?)
-                       """,
-                       (
-                           direction.from_code,
-                           direction.to_code,
-                           rate["changer_name"],
-                           rate["rate"],
-                           rate["exchange_rate"],
-                           rate["inmin"],
-                           rate["reserve"]
-                       ))
-    conn.commit()
-    conn.close()
+        CREATE TABLE IF NOT EXISTS rates (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    from_code TEXT,
+                    to_code TEXT,
+                    changer_name TEXT,
+                    rate REAL,
+                    exchange_rate REAL,
+                    inmin REAL,
+                    reserve REAL)
+                    """)
+        conn.commit()
+        conn.close()
+    ##########################################
+    def check_table(self):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("PRAGMA table_info(rates)")
+
+        for column in cursor.fetchall():
+            print(column)
+        
+        conn.commit()
+        conn.close()
+    ##########################################
+    def save_direction(self, direction):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        for rate in direction.rates:
+            cursor.execute("""
+                INSERT INTO rates(
+                        from_code,
+                        to_code,
+                        changer_name,
+                        rate,
+                        exchange_rate,
+                        inmin,
+                        reserve
+                        )
+                        VALUES(?,?,?,?,?,?,?)
+                        """,
+                        (
+                            direction.from_code,
+                            direction.to_code,
+                            rate["changer_name"],
+                            rate["rate"],
+                            rate["exchange_rate"],
+                            rate["inmin"],
+                            rate["reserve"]
+                        ))
+        conn.commit()
+        conn.close()
+    ##########################################
+    def load_direction(self, from_code, to_code):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("""
+                    SELECT
+                    changer_name,
+                    rate,
+                    exchange_rate,
+                    inmin,
+                    reserve
+                    FROM rates
+                    WHERE from_code = ?
+                    AND to_code = ?
+                """,
+                (
+                    from_code,
+                    to_code
+                ))
+        rows = cursor.fetchall()
+        conn.close()
+        rates = []
+        for row in rows:
+            rates.append({
+                "changer_name": row[0],
+                "rate": row[1],
+                "exchange_rate": row[2],
+                "inmin": row[3],
+                "reserve": row[4]
+            })
+        return ExchangeDirection(
+            from_code,
+            to_code,
+            rates
+        )
+    ##########################################
+    def delete_direction(self, from_code, to_code):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("""
+                        DELETE FROM rates
+                        WHERE from_code = ?
+                        AND to_code = ?    
+                    """,
+                    (
+                        from_code,
+                        to_code
+                    ))
+        conn.commit()
+        conn.close()
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-from api import get_rates
+# from api import get_rates
 from logic import calculate_reverse_spreads
 from gui import run_app
-from database import create_tables, save_direction, check_table
+from database import Database
 
 def main():
-    create_tables()
-    check_table()
+    db = Database()
+    db.create_tables()
+    db.check_table()
     def handle_search(from_code, 
                       to_code, 
                       show_reverse_rates_enabled=False, 
                       calculate_spreads_enabled=False):
-        direct_direction, reverse_direction = get_rates(from_code, to_code)
+        
+        direct_direction = db.load_direction(from_code, to_code)
+        reverse_direction = db.load_direction(to_code, from_code)
+        # direct_direction, reverse_direction = get_rates(from_code, to_code)
         # print(f"main.py direct direction -> {direct_direction.rates} \n"
         #       f"reverse direction --> {reverse_direction.rates}")
-        save_direction(direct_direction)
-        save_direction(reverse_direction)
+        # save_direction(direct_direction)
+        # save_direction(reverse_direction)
+        # test = load_direction(from_code, to_code)
+        # print("test rezult", test)
+        # print("test rates rezult", test.rates[:2])
         direct_direction.select_best(top=3)
         if show_reverse_rates_enabled or calculate_spreads_enabled:
             reverse_direction.select_cheapest(top=3)
